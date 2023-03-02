6ed7750b2d2c87b42b534209a4e83e2458f9c164
BriggsPaul <paul.adrien.briggs@gmail.com>
2023-03-02 16:13:45 -0500

    initial commit create-t3-app typescript and tailwind only

---
+++ clipboard-website/src/pages/index.tsx
@@ -0,0 +1,12 @@
+import { type NextPage } from "next";
+import Head from "next/head";
+import Link from "next/link";
+
+const Home: NextPage = () => {
+  return(
+  <h1>
+    Test
+  </h1>);
+};
+
+export default Home;
